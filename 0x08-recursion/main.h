9ae16604c5412befdf90d6b3decd199071c2383c
DarcyValene <darcynyabagabo@gmail.com>
2023-07-08 13:56:01 -0700

    a

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -14,5 +14,6 @@ int is_prime_number(int n);
 int check_prime(int n, int div);
 int is_palindrome(char *s);
 int check_palindrome(char *s, int start, int end);
+int wildcmp(char *s1, char *s2);
 
 #endif /* MAIN_H */
